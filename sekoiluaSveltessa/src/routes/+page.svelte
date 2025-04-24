41b7c3c16b00d8adac4d6ce0412e38dc11256591
Aarne, Ylönen <ag3483@student.jamk.fi>
2025-04-24 10:12:10 +0300

    lorem tekstiä lisätty

--- sekoiluaSveltessa/src/routes/+page.svelte
+++ sekoiluaSveltessa/src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis a ut fuga nihil necessitatibus
+	unde illum ab consectetur? Voluptatibus similique asperiores labore enim necessitatibus maiores
+	ipsum debitis nihil cum cupiditate.
+</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
