3e9862ff652442ea3186c0b3c1344864cb0a1b77
tikexd <AG3552@student.jamk.fi>
2025-04-24 11:54:40 +0300

    button komponentti

---
+++ sekoiluaSveltessa/src/lib/components/Button.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	interface Props {
+		text: string;
+		onclick: () => void;
+		disabled?: boolean;
+	}
+
+	let { text, onclick, disabled = false }: Props = $props();
+</script>
+
+<button {onclick} {disabled}>{text}</button>
+
+<style>
+	button {
+		font-family: Arial, sans-serif;
+		font-size: 1.2rem;
+		margin: 10px;
+		padding: 10px 20px;
+		background-color: var(--main-color);
+		color: white;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		transition: background-color 0.3s ease;
+		min-width: 15rem;
+	}
+
+	button:hover {
+		background-color: rgba(0, 0, 0, 0.25);
+	}
+
+	button:disabled {
+		background-color: #cccccc;
+		cursor: not-allowed;
+	}
+</style>
--- sekoiluaSveltessa/src/routes/+page.svelte
+++ sekoiluaSveltessa/src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 	const randomTaulukko: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 </script>
 
 <h1>Otsikko</h1>
 
-<button
-	on:click={() => {
+<Button
+	onclick={() => {
 		const randomIndex = Math.floor(Math.random() * randomTaulukko.length);
 		const randomElement = randomTaulukko[randomIndex];
-		alert(randomElement);
-	}}>Random numero 1-10</button
->
+		alert(randomElement);}} text="Random numero 1-10"/>
 <h1>Welcome to SvelteKit</h1>
 <p>
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis a ut fuga nihil necessitatibus
